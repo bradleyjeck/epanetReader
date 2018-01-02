ad402e574ae9441d8753a9a9d5b01e269f3e411b
Bradley Eck <7848456+bradleyjeck@users.noreply.github.com>
2018-01-02 14:04:19 +0000

    rm test with bad file

--- tests/testthat/test_epanet.inp-s3.r
+++ tests/testthat/test_epanet.inp-s3.r
@@ -52,12 +52,6 @@ test_that("icdm.inp",{
 
 })
 
-test_that("ap1.inp",{
-  
-  net <- read.inp("ap1.inp") 
-  numPipes <- dim( net$Pipes)[1]
-  expect_equal( numPipes,6 ) 
-})
 
 
 context("summary.epanet.inp s3 object") 
