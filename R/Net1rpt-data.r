c75937dcc7dc285dde6e5b653121b4f338b70ece
Bradley Eck <7848456+bradleyjeck@users.noreply.github.com>
2018-01-02 12:14:07 +0000

    example checking Net1rpt

--- R/Net1rpt-data.r
+++ R/Net1rpt-data.r
@@ -1,6 +1,6 @@
 #************************************
 #
-#  (C) Copyright IBM Corp. 2015
+#  (C) Copyright IBM Corp. 2015, 2018
 #
 #  Author: Bradley J Eck
 #
@@ -8,12 +8,18 @@
 
 #' Epanet's Net1 Example
 #'
-#' A dataset created by reading the Net1.inp file
+#' A dataset created by reading the Net1.rpt file
 #' distributed with Epanet using this package's
-#' read.inp() function.
+#' read.rpt() function.
 #' 
 #' @name Net1rpt 
 #' @docType data
 #' @usage Net1rpt
-#' @format An object of class \code{epanet.rpt} created by \link{read.inp}.
+#' @format An object of class \code{epanet.rpt} created by \link{read.rpt}.
+#' @examples
+#' #confirm built-in dataset matches output of read.rpt
+#' rpt <- file.path( find.package("epanetReader"), "extdata","Net1.rpt") 
+#' n1r <- read.rpt(rpt)
+#' ok <- isTRUE( all.equal(Net1rpt, n1r))
+#' if( ok==FALSE) stop("built-in Net1rpt doesn't match read.rpt")
 NULL
