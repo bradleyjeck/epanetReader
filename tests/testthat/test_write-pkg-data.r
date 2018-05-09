a10b7cdba2ad1797cc088d614ab059bce5691e95
Bradley Eck <7848456+bradleyjeck@users.noreply.github.com>
2018-05-09 12:51:40 +0100

    just use files in testthat dir

--- tests/testthat/test_write-pkg-data.r
+++ tests/testthat/test_write-pkg-data.r
@@ -1,9 +1,7 @@
 context("write the data that ships with the package") 
 test_that("Net1",{
   
-  myInp <-  file.path( "../../inst/extdata/Net1.inp") 
-  
-  Net1 <- suppressWarnings( read.inp(myInp))
+  Net1 <- suppressWarnings( read.inp("Net1.inp"))
   
   expect_true( is.epanet.inp(Net1)) 
   save(Net1, file="Net1.rdata") 
@@ -12,9 +10,7 @@ test_that("Net1",{
 
 test_that("Net1rpt",{
   
-  myRpt <-  file.path( "../../inst/extdata/Net1.rpt") 
-  
-  Net1rpt <- suppressWarnings( read.rpt(myRpt))
+  Net1rpt <- suppressWarnings( read.rpt("Net1.rpt"))
   
   expect_true( is.epanet.rpt(Net1rpt)) 
   save(Net1rpt, file="Net1rpt.rdata") 
